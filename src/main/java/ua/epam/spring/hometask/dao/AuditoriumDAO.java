6b220dfb57e5eb940ca70652b4c31f1b627ea7ee
Vladyslava_Hubenko <v8991518V>
2018-07-04 14:37:05 +0300

    added conctructor and change Map on List

--- src/main/java/ua/epam/spring/hometask/dao/AuditoriumDAO.java
+++ src/main/java/ua/epam/spring/hometask/dao/AuditoriumDAO.java
@@ -2,9 +2,7 @@ package ua.epam.spring.hometask.dao;
 
 import ua.epam.spring.hometask.domain.Auditorium;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -12,14 +10,18 @@ import java.util.Optional;
  */
 public class AuditoriumDAO {
 
-    private static Map<Long, Auditorium> auditoriumMap;
+    private static List<Auditorium> auditoriums;
+
+    public AuditoriumDAO(List<Auditorium> auditoriumsCons) {
+        auditoriums = auditoriumsCons;
+    }
 
     public List<Auditorium> getAll() {
-        return new ArrayList<>(auditoriumMap.values());
+        return auditoriums;
     }
 
     public Auditorium getByName(String name) {
-        Optional<Auditorium> auditorium = auditoriumMap.values().stream()
+        Optional<Auditorium> auditorium = auditoriums.stream()
                 .filter(item -> item.getName().equals(name)).findFirst();
         return auditorium.orElse(null);
     }
