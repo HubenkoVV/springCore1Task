38312ccb2809c15216d3986e5a97aa6dd092ba24
Vladyslava_Hubenko <v8991518V>
2018-07-04 14:35:27 +0300

    added new messages

--- src/main/java/ua/epam/spring/hometask/view/View.java
+++ src/main/java/ua/epam/spring/hometask/view/View.java
@@ -1,8 +1,6 @@
 package ua.epam.spring.hometask.view;
 
-import ua.epam.spring.hometask.domain.Ticket;
-
-import java.util.NavigableSet;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
 /**
@@ -20,6 +18,15 @@ public class View {
     public static final String SURNAME = "enter.surname";
     public static final String MAIL = "enter.mail";
     public static final String BIRTH = "enter.birthday";
+    public static final String CHOOSE_EV = "choose.event";
+    public static final String CHOOSE_DATE = "choose.date";
+    public static final String DATES = "dates";
+    public static final String AUDITORIUM = "auditorium";
+    public static final String SEATS = "seats";
+    public static final String CHOOSE_SEAT = "choose.seat";
+    public static final String THATS_ALL = "thats.all";
+    public static final String VIP = "VIP";
+    public static final String EXIT = "exit";
     private ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
 
     public void print(String... messages) {
@@ -28,7 +35,13 @@ public class View {
         }
     }
 
-    public void printList(NavigableSet<Ticket> objects) {
+    public void printMessages(String... messages) {
+        for (String mes : messages) {
+            System.out.println(mes);
+        }
+    }
+
+    public void printList(Collection<?> objects) {
         objects.forEach(o -> System.out.println(o.toString()));
     }
 }
